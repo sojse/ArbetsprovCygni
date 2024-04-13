e1a0a4eea2ca9753259618e4798b58af20bb8405
Josefine Isberg <95719327+sojse@users.noreply.github.com>
2024-04-13 13:58:29 +0200

    Add game class

---
+++ RockPaperScissorAPI/Models/Domain/Game.cs
@@ -0,0 +1,17 @@
+﻿using RockPaperScissorsAPI.Models.Enums;
+
+namespace RockPaperScissorsAPI.Models.Domain;
+
+public class Game
+{
+    public required Guid Id { get; set; }
+
+    public required List<Player> Players { get; set; }
+
+    public Player? Winner { get; set; }
+
+    public required GameState State { get; set; }
+
+    public GameResult? Result { get; set; }
+
+}
