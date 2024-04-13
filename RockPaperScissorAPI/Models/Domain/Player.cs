7a01fc9ad2cf971c7af683bb4c3382e69e7fc0a5
Josefine Isberg <95719327+sojse@users.noreply.github.com>
2024-04-13 13:58:23 +0200

    Add player class

---
+++ RockPaperScissorAPI/Models/Domain/Player.cs
@@ -0,0 +1,9 @@
+﻿using RockPaperScissorsAPI.Models.Enums;
+
+namespace RockPaperScissorsAPI.Models.Domain;
+
+public class Player
+{
+    public required string Name { get; set; }
+    public MoveType? Move { get; set; }
+}
