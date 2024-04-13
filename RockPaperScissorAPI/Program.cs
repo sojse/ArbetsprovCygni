b150941c0c9ff7271138506724871f6996c4d8a5
Josefine Isberg <95719327+sojse@users.noreply.github.com>
2024-04-13 14:08:07 +0200

    Add swagger configuration

--- RockPaperScissorAPI/Program.cs
+++ RockPaperScissorAPI/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.OpenApi.Models;
+using System.Reflection;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +7,20 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Rock Paper Scissors",
+        Version = "1.0"
+    });
+
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+
+});
 
 var app = builder.Build();
 
