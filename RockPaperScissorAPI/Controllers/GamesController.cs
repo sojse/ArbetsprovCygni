434b76e4a3f18fff5be8d562b114fc5ddccbbbd7
Josefine Isberg <95719327+sojse@users.noreply.github.com>
2024-04-13 14:09:44 +0200

    Add games controller

---
+++ RockPaperScissorAPI/Controllers/GamesController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RockPaperScissorAPI.Models.DTO;
+using RockPaperScissorAPI.Services;
+
+namespace RockPaperScissorAPI.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class GamesController : Controller
+{
+
+    private readonly IGameService _gameService;
+
+    public GamesController(IGameService gameService)
+    {
+        _gameService = gameService;
+    }
+
+    /// <summary>
+    /// Create a game and adding player one into the game
+    /// </summary>
+    /// <returns>The ID of the created game</returns>
+    [HttpPost]
+    [Consumes("application/json")]
+    [Produces("application/json")]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public ActionResult<GameResponseDto> CreateGame(GameRequestDto request)
+    {
+
+        try
+        {
+            var newGame = _gameService.CreateGame(request.Player);
+
+            var gameResponse = new GameResponseDto
+            {
+                Id = newGame,
+            };
+
+            return Created("Game was created!", gameResponse);
+
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+
+
+
+}
