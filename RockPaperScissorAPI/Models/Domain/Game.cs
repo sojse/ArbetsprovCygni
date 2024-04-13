a69ca073f306b883c0d6fe0ebfbc14b830ab9cf8
Josefine Isberg <95719327+sojse@users.noreply.github.com>
2024-04-13 15:32:45 +0200

    Add player 1 and player 2 fields in game class

--- RockPaperScissorAPI/Models/Domain/Game.cs
+++ RockPaperScissorAPI/Models/Domain/Game.cs
@@ -6,7 +6,9 @@ public class Game
 {
     public required Guid Id { get; set; }
 
-    public required List<Player> Players { get; set; }
+    public required Player Player1 { get; set; }
+
+    public Player? Player2 { get; set; }
 
     public Player? Winner { get; set; }
 
