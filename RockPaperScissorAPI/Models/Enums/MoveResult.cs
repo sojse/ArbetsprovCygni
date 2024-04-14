c1506b112677aa9ddd6b5c8151e5f9b577952ea4
Josefine Isberg <95719327+sojse@users.noreply.github.com>
2024-04-14 11:05:30 +0200

    Add move result enum

---
+++ RockPaperScissorAPI/Models/Enums/MoveResult.cs
@@ -0,0 +1,12 @@
+﻿namespace RockPaperScissorAPI.Models.Enums;
+
+public enum MoveResult
+{
+    Success,
+    GameNotFound,
+    PlayerNotFound,
+    InvalidMove,
+    PlayerAlreadyMoved,
+    GameFinished,
+    InvalidRequest
+}
