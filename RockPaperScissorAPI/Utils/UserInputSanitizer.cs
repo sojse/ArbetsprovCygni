9501bd2c49122a4c02a84ed93bceee661890a07f
Josefine Isberg <95719327+sojse@users.noreply.github.com>
2024-04-14 12:27:44 +0200

    Create user input sanitizer

---
+++ RockPaperScissorAPI/Utils/UserInputSanitizer.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Encodings.Web;
+
+namespace RockPaperScissorAPI.Utils;
+
+public class UserInputSanitizer
+{
+    public static string Sanitize(string input)
+    {
+        return HtmlEncoder.Default.Encode(input);
+    }
+}
