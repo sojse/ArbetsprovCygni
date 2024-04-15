97a80862fa955a9f8db462c282f73771e917390a
Josefine Isberg <95719327+sojse@users.noreply.github.com>
2024-04-15 20:02:27 +0200

    Create game service test setup

---
+++ RockPaperScissorsTest/GameServiceTests.cs
@@ -0,0 +1,21 @@
+﻿using Moq;
+using RockPaperScissorAPI.Models.Domain;
+using RockPaperScissorAPI.Models.Enums;
+using RockPaperScissorAPI.Services;
+using RockPaperScissorAPI.Services.Interfaces;
+
+namespace RockPaperScissorsTest;
+
+public class GameServiceTests
+{
+    private readonly Mock<IGameRepository> _mockRepository;
+    private readonly IGameService _gameService;
+
+    public GameServiceTests()
+    {
+        _mockRepository = new Mock<IGameRepository>();
+        _gameService = new GameService(_mockRepository.Object);
+    }
+
+}
+
