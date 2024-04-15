bc57c0147e543c0730db65d716aa9f9b1dcee31b
Josefine Isberg <95719327+sojse@users.noreply.github.com>
2024-04-15 08:28:49 +0200

    Add get game tests

--- RockPaperScissorsTest/GamesControllerTests.cs
+++ RockPaperScissorsTest/GamesControllerTests.cs
@@ -15,4 +15,33 @@ public class GamesControllerTests
         _mockService = new Mock<IGameService>();
         _controller = new GamesController(_mockService.Object);
     }
+
+    [Fact]
+    public async Task GetGame_ReturnsGameGameId_WhenGameExists()
+    {
+
+        Guid gameId = Guid.NewGuid();
+        string expectedGame = "Some game data";
+        _mockService.Setup(x => x.GetGameById(gameId)).ReturnsAsync(expectedGame);
+
+        var result = await _controller.GetGame(gameId);
+
+        Assert.IsType<OkObjectResult>(result);
+        var okResult = result as OkObjectResult;
+        var gameResponse = okResult.Value as string;
+        Assert.NotNull(gameResponse);
+        Assert.Equal(expectedGame, gameResponse);
+    }
+
+    [Fact]
+    public async Task GetGame_ReturnsNotFound_WhenGameDoesNotExist()
+    {
+        Guid gameId = Guid.NewGuid();
+        string nullValue = null;
+        _mockService.Setup(x => x.GetGameById(gameId)).ReturnsAsync(nullValue);
+
+        var result = await _controller.GetGame(gameId);
+
+        Assert.IsType<NotFoundResult>(result);
+    }
 }
